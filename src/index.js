0549b15767bea461afd3f6485f8542c28ff9a1d7
Mantuok <etigiloff@gmail.com>
2021-03-09 22:23:50 +0300

    init: typical-arrays-problem done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array !== 'object' || array.length === 0) return 0;
+  let result = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(result > array[i]) result = array[i];
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array !== 'object' || array.length === 0) return 0;
+  let result = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(result < array[i]) result = array[i];
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array !== 'object' || array.length === 0) return 0;
+  let result = 0;
+  for(let i = 0; i < array.length; i++){
+    result += array[i];
+  }
+  return result / array.length;
 }
